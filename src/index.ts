a924dd80f036029542cd54d5a960b569c61bed0a
Nirbhay Singh <nirbhaymahan100@gmail.com>
2024-10-25 07:53:34 +0530

    Bull mq based redis and queue infra up and running

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, {Express} from "express";
 import serverConfig from "./config/serverConfig";
 import apiRouter from "./routes";
+import sampleQueueProducer from "./producers/sampleQueueProducer";
+import SampleWorker from "./workers/sampleWorker";
 
 const app: Express = express();
 
@@ -9,5 +11,12 @@ app.use("/api", apiRouter);
 app.listen(serverConfig.PORT, () => {
     console.log(`server is running on port ${serverConfig.PORT}`);
     console.log("Hello world");
+
+    SampleWorker("SampleQueue");
+    sampleQueueProducer("SampleJob", {
+        name: "nirbhay",
+        college: "chandigarh university",
+        branch: "cse"
+    })
      
 })
